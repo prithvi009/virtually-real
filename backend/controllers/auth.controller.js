9ec4d8bd4be3e795a479f3a3e23dbf836baa671f
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-09-30 23:35:58 +0530

    signin route created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const signup= async(req,res, next)=>{
     const {username,email,password}=req.body;
@@ -18,6 +19,26 @@ export const signup= async(req,res, next)=>{
     }catch(err){
         next(err);
     }
+}
 
+export const signin = async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return res.status(400).json("Wrong credentials");
+        }
+        const validPassword = await bcrypt.compare(password,validUser.password);
+        if(!validPassword){
+            return res.status(400).json("Wrong credentials");
+        }
+        const token = jwt.sign({_id:validUser._id}, process.env.SECRET_KEY, {expiresIn:"1h"});
+        res.cookie("token",token,{
+            httpOnly:true,
+        }).status(200).json({success:true,token});
 
+    }
+    catch(err){
+        next(err);
+    }
 }
