3cab9dab37cd58a03d1a2ad3900dfe0784127285
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-09-30 20:34:25 +0530

    backend created

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
