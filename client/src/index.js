4c696ced3628bcf09efea3f4c9ac66d40625a377
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-10-01 01:47:00 +0530

    added redux persis

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './state/store';
+import {store, persistor} from './state/store';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,12 +1,31 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import userReducer from './users/userSlice';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
+
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
     reducer: {
-        user: userReducer,
+        user: persistedReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
 
