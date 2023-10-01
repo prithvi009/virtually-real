840b3a96ae604c9bf432a6e3a63697680209e471
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-10-01 15:27:36 +0530

    profile page ui created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,9 +26,9 @@ const Header = () => {
                 <Link to="/about">
                     <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
                 </Link>
-                <Link to="/sign-in">
+                <Link to="/profile">
                     {currentUser ? (
-                        <img src={currentUser.avatar} alt="profile" />
+                        <img className='rounded-full h-7 w-7  object-cover' src={currentUser.avatar} alt="profile" />
                     ):
                         <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Sign In</li>
                     }
