33ed9b42cea7e69c063a89a53f01659888f4e46b
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-10-05 22:01:15 +0530

    update user redux function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,6 +7,7 @@ import {getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import {app } from '../firebase'
 import { getDownloadURL } from 'firebase/storage'
 import { useDispatch } from 'react-redux'
+import { updateUserFailure, updateUserStart, updateUserSuccess } from '../state/users/userSlice'
 
 
 const Profile = () => {
@@ -49,11 +50,38 @@ const Profile = () => {
         }
     );
   }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/users/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7 '>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
         <img onClick={()=> fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
         <p className='text-sm self-center'>
@@ -69,9 +97,9 @@ const Profile = () => {
             ''
           )}
         </p>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id="username"/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id="email"/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id="password"/>
+        <input type="text" placeholder='username' defaultValue={currentUser.username} className='border p-3 rounded-lg' onChange={handleChange} id="username"/>
+        <input type="email" placeholder='email' defaultValue={currentUser.email} className='border p-3 rounded-lg' onChange={handleChange} id="email"/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' onChange={handleChange} id="password"/>
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
       </form>
       <div className='flex justify-between mt-5'>
